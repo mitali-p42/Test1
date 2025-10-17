a86411eec977d6d874cd2e3fd39d7ddf07aa82bf
Mitali Potnis <113000033+mitali-p42@users.noreply.github.com>
2025-10-17 10:35:09 -0400

    Add dummy Interview page

---
+++ Interview.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Interview() {
+  return (
+    <div style={{ padding: 24, textAlign: 'center' }}>
+      <h1>Hey 👋</h1>
+      <p>Welcome to your interview page!</p>
+      <p>We’ll get you started soon.</p>
+    </div>
+  );
+}
